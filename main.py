3c4a18450b280651e5569880ab7901c2d9c2e1bb
Andrei Serban <noskillz05@gmail.com>
2015-04-25 12:02:12 +0300

    now using sqlite instead of text file

--- main.py
+++ main.py
@@ -1,50 +1,16 @@
 __author__ = 'VerDe'
 '''
 Features:
-- Read from file - filter by date
-- Write to file - insert date
+- Accept input from user and inserts it into a local database
+- Read from the local database
 '''
-from datetime import datetime
+
 import sqlite3
 
 
-def read_entry():   # find
-    f = open("journal.txt", "r")
-    lines = f.readlines()
-    f.close()
-    entry_list = {}
-    entry_count = 0  # number of entries found. will be used as keys (string) in entry_list dictionary
-
-    for line in lines:
-        if line.find("Entry date") == 0:    # find the start of an entry
-            entry_count += 1
-            entry_list[str(entry_count)] = line  # add the Entry date lines in the entry_list dict, to be filtered later
-
-    if entry_list:  # if dictionary not empty
-        print("\nTable of contents:")
-        for k, v in entry_list.items():  # print keys and values in entry_list as a TOC
-            print(k, v, end="")
-        print()
-    else:
-        print("There are no entries. Maybe you should write one...\n")
-        return
-
-    selected_entry = input("Select an entry from the list: ")  # prompt to pick a pick from entry_list
-    if selected_entry in entry_list.keys():
-        copy = False
-        entry = "\n" + str(entry_list[selected_entry]) + "\n"
-        s = entry_list[selected_entry]
-        for line in lines:
-            if line == s:
-                copy = True
-            elif line == "*** END ENTRY ***":
-                copy = False
-            elif copy:
-                entry += line
-    print(entry)
-
-
-def write_entry():  # write line by line to a string, to be appended to file through insert_entry
+#  Accepts input from the user and manipulates it.
+#  The input will be inserted in the sqlite db through the insert_entry function
+def write_entry():
     writing = True
     entry = ""
     print("What's on your mind?\nTip: type 'done' to finish writing the entry.\n")
@@ -54,24 +20,60 @@ def write_entry():  # write line by line to a string, to be appended to file thr
             print()
             writing = False
         else:
-            entry += entry_row
+            entry += entry_row + "\n"
     if entry != "":
         return entry
 
 
-def select_entry(s):
+#  Accepts input from the user and checks to see if the selected entry id exists in the database
+#  Returns the id if valid.
+#  ID is passed to the get_entry function
+def choose_entry():
+        conn = sqlite3.connect("Journal.db")
+        cur = conn.cursor()
+        entry_list = []
+        done = False
+        while not done:
+            try:
+                cur.execute('''SELECT ID, EntryDate FROM Entries''')
+            except sqlite3.OperationalError:
+                print("You have no entries yet.\n")
+                break
+
+            print("Entry list:")
+            for row in cur.fetchall():
+                print("{0}    {1}".format(row[0], row[1]), end="")
+                if str(row[0]) not in entry_list:
+                    entry_list.append(str(row[0]))
+                print()
+            user_pick = input("Choose an entry, or type 'done': ")
+            if user_pick == "done":
+                break
+            else:
+                if str(user_pick) not in entry_list:
+                    print("Not a valid entry.\n")
+                    continue
+                else:
+                    print()
+                    conn.close()
+                    done = True
+                    return user_pick
+
+
+#  Returns chosen entry from the local database
+def get_entry(s):
     conn = sqlite3.connect("Journal.db")
     cur = conn.cursor()
     try:
-        cur.execute('''SELECT Entry FROM Entries WHERE ID=?;''', s)
+        cur.execute('''SELECT Entry FROM Entries WHERE ID = ?;''', (str(s),))
         row = cur.fetchone()
         if row:
-            print(row)
+            output_entry = row[0]
+            print(output_entry)
         else:
             print("Invalid entry.\n")
     except sqlite3.OperationalError:
         print("No entries yet. Maybe you should write one...\n")
-
     conn.close()
 
 
@@ -92,12 +94,13 @@ def insert_entry(s):    # writes a string collected by write_entry
 
 # main program starts here
 # pick read / write mode, or quit
-done = False
-while not done:
+running = True
+while running:
     mode = input("Do you wish to read or write?\nTip: type 'quit' to quit.\n")
     if mode.lower() == "read":
-        select_entry("1")
-        # read_entry()
+        chosen_entry = choose_entry()
+        if chosen_entry:
+            get_entry(chosen_entry)
         continue
     if mode.lower() == "write":
         written_entry = write_entry()
@@ -105,6 +108,6 @@ while not done:
             insert_entry(written_entry)
         continue
     if mode.lower() == "quit":
-        done = True
+        running = False
     else:
         print("Invalid input. Accepted values are 'read', 'write' or 'quit'\n")
